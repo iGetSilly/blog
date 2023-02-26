6a37b066767a4b3f93c786872ad9f058c7ac916d
Leonardo Dutra <iuoyyp@gmail.com>
2023-02-26 20:13:43 -0300

    added the components props

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -2,10 +2,21 @@ import Post from "../Post"
 
 function Article() {
     return (
-        <>
-            <h1>Article.js</h1>
-            <Post />
-        </>
+        <section className="container">
+            <h1>Article</h1>
+            <div className=" mt-5 container-posts">
+                <Post subtitle="tecnologia" title="O Guia definitivo do blog">
+                    Texto de teste 1
+                </Post>
+                <Post subtitle="fotografia" title="Quais as melhores câmeras em 2023?">
+                    Texto de teste 2
+                </Post>
+                <Post subtitle="cinema" title="Os 10 filmes com maior bilheteria">
+                    Texto de teste 3
+                </Post>
+            </div>
+            
+        </section>
         
     )
 }
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,9 +1,21 @@
-function Post() {
-    
-    
-    
+const Post = (props) => { //também é possível desestruturar Exemplo: em vez de props colocar: (subtitle, title, children) irá funcionar da mesma forma e não será necessario usar o props.subtitle por exeplo somente subtitle
     return (
-        <h1>Post.js</h1>
+        <div className="post mx-2">
+            <div className="img-post"></div>
+            <h5>{props.subtitle}</h5>
+            <h3>{props.title}</h3>
+            <p className="mt-1">
+                {props.children}
+            </p>
+            <div className="flex pt-2">
+                <div className="img-profile"></div>
+                <div className="desc-profile ml-2">
+                    <h6 className="color-blue">Fulano de Tal</h6>
+                    <p> Aug 2, 2020 - 8 min read</p>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
